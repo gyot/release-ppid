6be9ce10de28f4694b7261f4c3a4eeef63c306f2
Wahyu Ramdhani <wahyuramdhani.ti@gmail.com>
2025-10-16 18:15:52 +0800

    Menambahkan data regulasi, Layanan, dan Informasi serta biodata pejabat

--- assets/channel-CN9d91kK.js
+++
@@ -1,1 +0,0 @@
-import{ap as o,aq as n}from"./index-n63KXdrQ.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ assets/clone-BcHwUroZ.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-CdI6cDWx.js";var e=4;function a(o){return r(o,e)}export{a as c};
